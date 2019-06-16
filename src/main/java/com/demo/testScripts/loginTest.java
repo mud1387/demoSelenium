a492123751c9b1ddc98a455046a301b170a426b5
Chowdhary <mudit.chowdhary@capgemini.com>
2019-06-16 10:34:44 -0400

    Demo

---
+++ src/main/java/com/demo/testScripts/loginTest.java
@@ -0,0 +1,81 @@
+package com.demo.testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class loginTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		// objects and variables instantiation
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mchowdha\\eclipse-workspace\\demo\\src\\resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String appUrl = "http://automationpractice.com/index.php";
+		
+
+		// launch the chrome browser and open the application url
+
+		driver.get(appUrl);
+		// maximize the browser window
+
+		driver.manage().window().maximize();
+		// declare and initialize the variable to store the expected title of the
+		// webpage.
+
+		String expectedTitle = "My Store";
+		// fetch the title of the web page and save it into a string variable
+
+		String actualTitle = driver.getTitle();
+		// compare the expected title of the page with the actual title of the page and
+		// print the result
+
+		if (expectedTitle.equals(actualTitle))
+		{
+			System.out.println("Verification Successful - The correct title is displayed on the web page.");
+		}else{
+			System.out.println("Verification Failed - An incorrect title is displayed on the web page.");
+		}
+
+		WebElement signIn = driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a"));
+		
+		signIn.click();
+		
+		WebElement loginEmailAddress= driver.findElement(By.xpath("//*[@id='email']"));
+		
+		if (loginEmailAddress.isDisplayed()) {
+			
+			loginEmailAddress.clear();
+			loginEmailAddress.sendKeys("abcd@gmail.com");
+		}		
+		
+		WebElement loginPassword = driver.findElement(By.xpath("//*[@id='passwd']"));
+		
+		loginPassword.clear();
+		loginPassword.sendKeys("1234");
+		
+		WebElement submitButton = driver.findElement(By.id("SubmitLogin"));
+		submitButton.click();
+		
+		WebElement errorAlert= driver.findElement(By.xpath("//li[contains(text(),'Invalid password.')]"));
+		
+		if (errorAlert.isDisplayed()) {
+			
+			System.out.println("Alert error found");
+		}
+		// close the web browser
+		
+		Thread.sleep(500);
+
+		driver.close();
+
+		System.out.println("Test script executed successfully.");
+
+		// terminate the program
+
+		System.exit(0);
+
+	}
+
+}
